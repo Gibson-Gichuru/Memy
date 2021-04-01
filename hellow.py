bba8890a48b68d50693621cfbc1a5cf0705d3ef9
Gibson-Gichuru <gichuru.developer@gmail.com>
2021-04-01 15:48:38 +0300

    Initial commit

---
+++ hellow.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name = name)
+
+
+@app.errorhandler(400)
+def page_not_found(e):
+
+	return render_template('400.html') 400
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+
+	return render_template('500.html') 500
+
+if __name__ == '__main__':
+
+    app.run(debug = True)
