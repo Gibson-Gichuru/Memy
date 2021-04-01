da3100dfacfb47d5aea35a47dc4568ea7a14acb6
Gibson-Gichuru <gichuru.developer@gmail.com>
2021-04-01 16:49:17 +0300

    template restructuring

--- hellow.py
+++ hellow.py
@@ -14,16 +14,16 @@ def user(name):
     return render_template('user.html', name = name)
 
 
-@app.errorhandler(400)
+@app.errorhandler(404)
 def page_not_found(e):
 
-	return render_template('400.html') 400
+	return render_template('400.html') ,404
 
 
 @app.errorhandler(500)
 def internal_server_error(e):
 
-	return render_template('500.html') 500
+	return render_template('500.html') ,500
 
 if __name__ == '__main__':
 
