51a2495c7dd51482677c2e3661197ec6b89504d1
Gibson-Gichuru <gichuru.developer@gmail.com>
2021-04-02 11:54:21 +0300

    form implimentation with flask-wtf

--- hellow.py
+++ hellow.py
@@ -1,13 +1,34 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
+
+
+import datetime
 
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
-@app.route('/')
+app.config['SECRET_KEY'] = 'dfwwe34.-435435pdfgl6u7were35]pty[py.ng;tyu-54735'
+@app.route('/', methods = ['GET', 'POST'])
 def home():
-    return render_template('home.html')
+
+	name = None
+
+	form = NameForm()
+
+	if form.validate_on_submit():
+
+		name = form.name.data
+
+		form.name.data = ''
+	return render_template('home.html', form = form, name = name)
 
 @app.route('/user/<name>')
 def user(name):
@@ -24,6 +45,14 @@ def page_not_found(e):
 def internal_server_error(e):
 
 	return render_template('500.html') ,500
+
+
+
+
+class NameForm(Form):
+
+	name = StringField('what is your name', validators = [Required()])
+	submit = SubmitField('submit')
 
 if __name__ == '__main__':
 
