7a1ba5069c5f17d94916e62403c811b2c03abd54
Gibson-Gichuru <gichuru.developer@gmail.com>
2021-04-02 19:47:03 +0300

    message flashing

--- hellow.py
+++ hellow.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 
@@ -22,7 +22,15 @@ def home():
 	form = NameForm()
 
 	if form.validate_on_submit():
+
+		old_name = session.get('name')
+
+		if old_name is not None and old_name != form.name.data:
+
+			flash("Looks like you have changed your name")
+
 		session['name']= form.name.data
+		form.name.data = ''
 		return redirect(url_for('home'))
 	return render_template('home.html', form = form, name = session.get('name'))
 
