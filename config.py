45b7dc6681958c2410aee414d0a0d6bc8a115bee
Gibson-Gichuru <gichuru.developer@gmail.com>
2021-04-06 20:56:28 +0300

    Applicatoin configuraton setup

---
+++ config.py
@@ -0,0 +1,48 @@
+import os
+
+basedir = os.path.abspath(so.path.dirname(__file__))
+
+class Config:
+	
+	SECRET_KEY = os.environ.get('SECRET_KEY') 
+
+	SQLALCHEMY_COMMITON_TEARDOWN = True
+	FLASK_MAIL_SUBJECT_PREFIX = '[Flasky]'
+	FLASK_MAIL_SENDER = 'Flask Admin <flaskyadmin@gmail.com>'
+	FLASK_ADMIN = os.environ.get('FLASK_ADMIN')
+
+	@staticmethod
+	def init_app(app):
+		pass
+
+class DevelopmentConfig(Config):
+	
+	DEBUG = True
+
+	MAIL_SERVER = 'smtp.googlemail.com'
+	MAIL_PORT = 587
+	MAIL_USE_TSL = True
+	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+	'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+class TestingConfig(Config):
+
+	TESTING = True
+
+	SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+	'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+
+class ProductionConfig(Config):
+
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+	'sqlite:///' + os.path.join(basedir, 'data.sqlte')
+
+
+
+config = dict(developent = DevelopmentConfig, 
+	testing = TestingConfig, 
+	production = ProductionConfig, 
+	default = DevelopmentConfig)
